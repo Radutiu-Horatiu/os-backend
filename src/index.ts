7b8d3ed4e530f10351a7258ac35f0ce018f5a939
Horatiu <horatiuradutiu@yahoo.com>
2025-02-04 16:50:39 +0200

    best practices

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ require('dotenv').config();
 
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
+import rateLimit from 'express-rate-limit';
 
 import './lib/db';
 import { walletAddressAuth } from './middlewares/user';
@@ -14,16 +16,32 @@ import userRoutes from './routes/user';
 const app = express();
 const port = process.env.PORT || 3333;
 
-// Enable CORS for all origins
-app.use(cors());
+// Configure rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+});
 
+// Apply rate limiting to all requests
+app.use(limiter);
+
+// Use helmet to set various HTTP headers for security
+app.use(helmet());
+
+// Swagger
 const swaggerDocs = swaggerJSDoc(swaggerOptions());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
+// CORS
+app.use(cors({ origin: process.env.ALLOWED_ORIGIN }));
+
+// Middleware
 app.use(express.json());
 app.use(express.raw({ type: 'application/vnd.custom-type' }));
 app.use(express.text({ type: 'text/html' }));
 
+// Routes
 app.use('/user', walletAddressAuth, userRoutes);
 
 app.listen(port, () => {
